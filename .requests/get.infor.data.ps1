0ad13eb24587a9439c9e4cfb5eb75a9ff80dfb69
Owen Ganza <o.ganza@alu.com>
2025-03-27 16:31:07 +0200

    updating

--- .requests/get.infor.data.ps1
+++
@@ -1,4 +0,0 @@
-$headers=@{}
-$headers.Add("x-rapidapi-key': 'a419c4e183mshd579c3b0521e74cp192608jsndc0b5c923c55")
-$headers.Add("x-rapidapi-host': 'tripadvisor16.p.rapidapi.com")
-$response = Invoke-WebRequest -Uri 'https://tripadvisor16.p.rapidapi.com/api/v1/flights/searchFlights' -Method GET -Headers $headers
